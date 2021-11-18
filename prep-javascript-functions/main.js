dada1ff03b2c65ed0e448486db6f7f18fbde92b3
Anthony Pineda <anthonydn99@gmail.com>
2021-11-18 11:18:45 -0800

    completed prep-javascript-functions multiplyAndDividebBy5

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -1,6 +1,6 @@
-function addAndMultiplyBy5(num1, num2) {
-  return num1 + num2;
+function multiplyAndDivideBy5(num1, num2) {
+  return num1 * num2;
 }
 
-var addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
-console.log(addAndMultiplyBy5Result * 5);
+var multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
+console.log(multiplyAndDivideBy5Result / 5);
