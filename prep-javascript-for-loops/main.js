7d06fb0ec1cb2963427cbecf4ada9b2746b45087
Anthony Pineda <anthonydn99@gmail.com>
2021-11-19 16:58:17 -0800

    completed prep-javascript-for-loops

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -1,2 +1,6 @@
 // create your loops here.
-for (let i = 0; i < 20; i += 2) { console.log(i); }
+for (let i = 0; i < 10; i++) { console.log(i); }
+
+for (let j = 0; j < 20; j += 2) { console.log(j); }
+
+for (let k = 100; k > 0; k--) { console.log('Time till explosion:' + k); }
